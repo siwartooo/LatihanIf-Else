5fe2dd002e6f664f3aa851fdf0b17942af260d7c
siwartooo <rafqiisalman1710@gmail.com>
2025-07-28 23:26:02 +0700

    Add CekAngka.js and CekAngkaSwitch.js examples

---
+++ CekAngka.js
@@ -0,0 +1,19 @@
+let hari = 3;
+
+if (hari === 1) {
+  console.log("Senin");
+} else if (hari === 2) {
+  console.log("Selasa");
+} else if (hari === 3) {
+  console.log("Rabu");
+} else if (hari === 4) {
+  console.log("Kamis");
+} else if (hari === 5) {
+  console.log("Jumat");
+} else if (hari === 6) {
+  console.log("Sabtu");
+} else if (hari === 7) {
+  console.log("Minggu");
+} else {
+  console.log("Input tidak valid");
+}
---
+++ CekAngkaSwitch.js
@@ -0,0 +1,27 @@
+let hari = 3;
+
+switch (hari) {
+  case 1:
+    console.log("Senin");
+    break;
+  case 2:
+    console.log("Selasa");
+    break;
+  case 3:
+    console.log("Rabu");
+    break;
+  case 4:
+    console.log("Kamis");
+    break;
+  case 5:
+    console.log("Jumat");
+    break;
+  case 6:
+    console.log("Sabtu");
+    break;
+  case 7:
+    console.log("Minggu");
+    break;
+  default:
+    console.log("Input tidak valid");
+}
